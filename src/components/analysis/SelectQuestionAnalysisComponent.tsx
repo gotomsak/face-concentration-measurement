0aa8923e918b1cc7b36ce6f5e80af7da4d135ae0
gotomsak <gotomsak@gmail.com>
2021-12-01 01:41:32 +0900

    分析コンポーネントを追加

---
+++ src/components/analysis/SelectQuestionAnalysisComponent.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+const SelectQuestionAnalysisComponent: React.FC=()=>{
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SelectQuestionAnalysisComponent
